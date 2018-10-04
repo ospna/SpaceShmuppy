c38442ce926d0cd14d0998b4b77258c012a51695
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2018-10-03 21:26:59 -0400

    Added Enemies

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set In Inspector")]
+    public float speed = 10f;       // the speed in m/s
+    public float fireRate = 0.3f;   // secd=onds per shot
+    public float health = 10;
+    public int score = 100;         // points earned for destroying
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    // Property : method that acts like a field
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+
+        if(bndCheck != null && bndCheck.offDown)
+        {
+            // check to make sure its gone off screen
+            if(pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+
+    // get current position and move it downward in the Y direction
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
